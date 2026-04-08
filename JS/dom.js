bf67dc63481aab0f05ef236187ef24b4d5f234a1
nishant <nishnattripathi@gmail.com>
2026-04-08 21:23:08 +0530

    dom Js

---
+++ JS/dom.js
@@ -0,0 +1,10 @@
+function result() {
+    let changeColor = document.getElementById('text');
+    changeColor.style.color = "red";
+    let bodyColor = document.getElementsByTagName('body')[0];
+    bodyColor.style.backgroundColor = "Blue";
+}
+let para=document.getElementById('paratext');
+function changetextcolor(){
+    para.style.backgroundcolor = "";
+}
